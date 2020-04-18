bc573d041856220690c2427aba0d0052f80dea4f
apena19 <andrespe1567@gmail.com>
2020-04-18 18:29:28 -0500

    done calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'calculadora';
+
+    current = '0';
+    numberState = false;
+    estadoDecimal = false;
+    beforeOperation = '';
+    newOperacion = '';
+
+    entervalue(v: string) {
+        if (this.numberState) {
+            this.current = v;
+            this.numberState = false;
+        } else {
+            this.current === '0' ? this.current = v : this.current += v;
+
+        }
+    }
+
+    decimal() {
+        if (!this.current.includes('.')) {
+            this.current += '.';
+        } else {
+            if (this.estadoDecimal) {
+                this.current += '.';
+                this.estadoDecimal = false;
+            }
+        }
+    }
+
+    condition(op: string) {
+
+        if (op == "=") {
+            const ultimoC = this.current.charAt(this.current.length - 1);
+            var re = new RegExp("([0-9])");
+
+            if (re.test(ultimoC)) {
+                const result = eval(this.current);
+                this.beforeOperation = this.current;
+                this.current = String(result);
+            } else {
+                alert("No es valida la operacion");
+                this.clear();
+            }
+
+        }else{
+            this.current += op
+            this.estadoDecimal = true;
+        }
+
+    }
+
+    clear() {
+        this.current = '0';
+        this.beforeOperation = '';
+        this.numberState = false;
+    }
+
 }
