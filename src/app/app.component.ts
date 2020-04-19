2bcb09b3d6428df80eb87764af19b0134735b377
apena19 <andrespe1567@gmail.com>
2020-04-18 19:38:59 -0500

    add keypress 90%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,67 @@ export class AppComponent  {
     beforeOperation = '';
     newOperacion = '';
 
+    keyPressNum(event: any) {
+      const pattern = /[0-9]/;
+      const inputChar = String.fromCharCode((event as KeyboardEvent).charCode);
+      if (!pattern.test(inputChar)) {  
+          event.preventDefault();
+      }
+      else{
+        if (this.numberState) {
+            this.current = inputChar;
+            this.numberState = false;
+        } else {
+            this.current === '0' ? this.current = inputChar : this.current += inputChar;
+        }
+      }
+    }
+
+    keyPressOperator(event: any) {
+      const pattern = /[-,+,*,/]/;
+      const inputChar = String.fromCharCode((event as KeyboardEvent).charCode);
+      if (!pattern.test(inputChar)) {  
+          event.preventDefault();
+      }
+      else{
+        if (inputChar == "=") {
+            const ultimoC = this.current.charAt(this.current.length - 1);
+            var re = new RegExp("([0-9])");
+
+            if (re.test(ultimoC)) {
+                const result = eval(this.current);
+                this.beforeOperation = this.current;
+                this.current = String(result);
+            } else {
+                alert("operacion invalida");
+                this.clear();
+            }
+
+        }else{
+            this.current += inputChar
+            this.estadoDecimal = true;
+        }
+      }
+    }
+
+    keyPressPunto(event: any) {
+      const pattern = /[.]/;
+      const inputChar = String.fromCharCode((event as KeyboardEvent).charCode);
+      if (!pattern.test(inputChar)) {  
+          event.preventDefault();
+      }
+      else{
+        if (!this.current.includes('.')) {
+            this.current += '.';
+        } else {
+            if (this.estadoDecimal) {
+                this.current += '.';
+                this.estadoDecimal = false;
+            }
+        }
+      }
+    }
+
     entervalue(value: string) {
         if (this.numberState) {
             this.current = value;
